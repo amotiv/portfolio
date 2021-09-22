69d39db0530ae18287c5252a9011df82cabb3949
amotiv <89550131+amotiv@users.noreply.github.com>
2021-09-22 17:31:31 -0500

    footer works

--- components/Footer.js
+++ components/Footer.js
@@ -8,14 +8,12 @@ function Footer() {
             <div className="flex">
                 <div className="h-48 sm:h-72 w-screen bg-gray-400">
                     <div className="flex bg-cover bg-face-img rounded-full my-12 mx-4 lg:mx-96 w-24 sm:w-36 h-1/2">
-                    <div className="flex flex-col h-1/2 w-1/4 mx-72 -my-4">
+                    <div className="flex flex-col h-1/2 w-1/4 mx-52 sm:mx-72 -my-4 text-black text-lg leading-10">
                         <p className="flex text-xl text-black font-semibold">Navigation</p>
-                        <p className="flex text-xl text-blue-500 font-semibold my-2">-></p>
-                        <p className="flex text-xl text-blue-500 font-semibold my-2">-></p> 
-                    </div>
-                    <div className="flex flex-col my-4 -mx-72 cursor-pointer text-black text-xl leading-10">
                         <Link href="/">Home </Link>
                         <button onClick={handleClick}>Github</button>
+                    </div>
+                    <div className="flex flex-col my-4 -mx-72 cursor-pointer text-black text-xl leading-10">
                         </div>
                         <div className="flex mx-48">
 
