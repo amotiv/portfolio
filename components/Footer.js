415ce30189dcc327ae0e632bfc42780078f7045d
amotiv <89550131+amotiv@users.noreply.github.com>
2021-09-23 11:10:47 -0500

    fixed github

--- components/Footer.js
+++ components/Footer.js
@@ -10,8 +10,10 @@ function Footer() {
                     <div className="flex flex-grow bg-cover bg-face-img rounded-full my-12 mx-4 lg:mx-96 w-24 sm:w-24 h-1/2">
                     <div className="flex flex-col h-1/2 w-1/4 mx-40 sm:mx-72 -my-4 text-black text-sm sm:text-lg leading-10">
                         <p className="flex text-lg sm:text-xl text-black font-semibold">Navigation</p>
+                        <div className="flex flex-col text-lg sm:text-xl text-black">
                         <Link href="/">Home </Link>
                         <button onClick={handleClick}>Github</button>
+                        </div>
                     </div>
                     <div className="flex flex-col my-4 -mx-72 cursor-pointer text-black text-xl leading-10">
                         </div>
